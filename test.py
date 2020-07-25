a2cddce4d9ce819c67765b533733d478af8e47ce
Sol Han <hans53400@naver.com>
2020-07-25 17:07:45 +0900

    첫커밋

---
+++ test.py
@@ -0,0 +1,7 @@
+
+print("안녕하세요")
+print("수정1")
+print("수정2")
+
+print("수정1")
+print("수정2")
---
+++ test2.py
@@ -0,0 +1,8 @@
+
+print("안녕하세요")
+print("수정1")
+print("수정2")
+print("수정3")
+
+
+print("수정3")print("수정3")print("수정3")
